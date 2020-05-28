acad1edf594c799ed29db99666080790a4987a27
YANG-JISO <glsu2004@gmail.com>
2020-05-28 22:20:30 +0900

    Apply Material UI

--- src/compoenent/customer.js
+++ src/compoenent/customer.js
@@ -1,43 +1,18 @@
 import React from 'react';//특정 라이브러리 호출
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     render(){ //React.Component 에 포함되는 함수로 항상 실행되는 함수
         return(
-            <div>
-                <CustomerProfile
-                    id={this.props.id}
-                    name={this.props.name}
-                    image={this.props.image}
-                />
-                <CustomerInfo 
-                    birth={this.props.birth}
-                    gender={this.props.gender}
-                    job={this.props.job}
-                />
-            </div>
-        )
-    }
-}
-
-class CustomerProfile extends React.Component{
-    render(){
-        return( 
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component{
-    render(){
-        return( 
-            <div>
-                <p>{this.props.birth}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birth}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
